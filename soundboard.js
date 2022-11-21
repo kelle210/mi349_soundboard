c2b3648e22f5fc304b1ca1bd61ec521844845c3b
Alexa Kelley <kelle210@msu.edu>
2022-11-20 23:30:16 -0500

    soundboard

--- soundboard.js
+++ soundboard.js
@@ -3,3 +3,15 @@ var yay = document.getElementById("yay")
 yay_button.addEventListener('click', function () {
     yay.play();
 })
+
+var bell_button = document.getElementById('bell_button')
+var bell = document.getElementById("bell")
+bell_button.addEventListener('click', function () {
+    bell.play();
+})
+
+var champagne_button = document.getElementById('champ_button')
+var champagne = document.getElementById("champagne")
+champagne_button.addEventListener('click', function () {
+    champagne.play();
+})
